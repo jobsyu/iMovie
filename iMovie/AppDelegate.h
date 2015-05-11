54001f696746aeaec8821d2189891e86d0b519d6
jobsyu <1175585627@qq.com>
2015-05-11 14:31:01 +0800

    Initial Commit

---
+++ iMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMovie
+//
+//  Created by qianfeng on 15/5/11.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
